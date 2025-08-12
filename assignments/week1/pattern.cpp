21cda545fc0053334d64e7217c07d011e246da8a
Pratyush Dhavala <pratyush.v.d@intimetec.com>
2025-08-13 00:31:39 +0530

    Made changes after review

--- assignments/week1/pattern.cpp
+++ assignments/week1/pattern.cpp
@@ -1,38 +1,76 @@
 #include <iostream>
 
-void pattern(char str[]);
-bool isNan(char num[]);
+void buildPattern(char str[]);
+bool isNotANumber(char num[]);
 int charArraySize(char num[]);
-void takeInput(char num[]);
+void takeInput();
 double pow(double base, double exponent);
 int charToNum(char num[]);
+void printMiddleRow(int num);
+bool isFirstHalf(int num, int col);
+void trim(char arr[]);
 
 int main() {
 
-    std::cout << "Please enter a number: " << std::endl;
-    char num[10];
-    std:: cin.getline(num, 9);
-    std::cout << '\n';
+    while(true) {
 
-    // checks for wrong input and asks for another input
-    while(isNan(num)) {
-        takeInput(num);
+        takeInput();
+
+        char continueOrNot[100];
+        std::cout << "Enter (Y/y) if you want to continue, or any other key to exit!" << '\n';
+        std::cin.getline(continueOrNot, 100);
+
+        trim(continueOrNot);
+
+        if (continueOrNot[0] != 'Y' && continueOrNot[0] != 'y') {
+            std::cout << "Thank you for making Patterns! You have exited the program." << '\n';
+            break;
+        }
+    }
+}
+
+void trim(char arr[]) {
+    int start = 0;
+
+    while (arr[start] == ' ' || arr[start] == '\t' || arr[start] == '\n') {
+        start++;
     }
 
-    // pattern logic function
-    pattern(num);
-    
+    int end = start;
+    while (arr[end] != '\0') {
+        end++;
+    }
+    end--; 
+
+    while (end >= start && (arr[end] == ' ' || arr[end] == '\t' || arr[end] == '\n')) {
+        end--;
+    }
+
+    int j = 0;
+    for (int i = start; i <= end; i++) {
+        arr[j++] = arr[i];
+    }
+    arr[j] = '\0'; 
 }
 
-// function to take input in case of wrong input
-void takeInput(char num[]) {
-    std::cout << "Wrong input! Please enter only a number: " << std::endl;
-    std:: cin.getline(num, 9);
-    std::cout << '\n';
+
+void takeInput() {
+    char num[100];
+    std::cout << "Please enter a number: " << std::endl;
+    std::cin.getline(num, 100);
+
+    trim(num);
+
+    if (isNotANumber(num)) {
+        std::cout << "Wrong input! ";
+        takeInput();
+    } else {
+        buildPattern(num);
+    }
+
 }
 
-// function to check if any character is not a number
-bool isNan(char num[]) {
+bool isNotANumber(char num[]) {
     
     int length = charArraySize(num);
 
@@ -48,7 +86,6 @@ bool isNan(char num[]) {
     return false;
 }
 
-// function to calculate the size of the char array
 int charArraySize(char num[]) {
 
     int size = 0;
@@ -60,21 +97,23 @@ int charArraySize(char num[]) {
     return size;
 }
 
-// function to convert char to num
 int charToNum(char num[]) {
 
-    int length = charArraySize(num);
+    int size = charArraySize(num);
     int result = 0;
 
-    for (int i = 0; i < length; i++) {
-        int n = num[i] - '0';
-        n = n * pow(10, length - i - 1);
-        result += n;
+    for (int i = 0; i < size; i++) {
+        int sum = num[i] - '0';
+        sum = sum * pow(10, size - i - 1);
+        result += sum;
     }
     return result;
 }
 
-// function to calculate exponents
+bool isEven(int num) {
+    return num % 2 == 0;
+}
+
 double pow(double base, double exponent) {
 
     double result = 1.0;
@@ -89,52 +128,116 @@ double pow(double base, double exponent) {
     return result;
 }
 
-// function to create the pattern, contains the pattern logic
-void pattern(char str[]) {
+bool isFirstHalf(int num, int col) {
+
+    if (isEven(num) && col < num / 2) {
+        return true;
+    } else if (isEven(num) && col >= num / 2) {
+        return false;
+    } else if (col <= num / 2) {
+        return true;
+    } else if (col >= (num / 2 + 1)) {
+        return false;
+    }
+}
+
+bool isNotMiddleRow(int col, int num) {
+    if (isEven(num) && col == num / 2) {
+        return false;
+    } else if (col == (num / 2 + 1)) {
+        return false;
+    }
+    return true;
+}
+
+void printEvenMiddleRow(int num) {
+    int i = 0;
+    while (i < num / 2) {
+        if (i % 2 == 0) {
+            std::cout << "1 ";
+        } else {
+            std::cout << "0 ";
+        }
+        i++;
+    }
+
+    i = 0;
+    while (i < num / 2) {
+        if (i % 2 == 0) {
+            std::cout << "1 ";
+        } else {
+            std::cout << "0 ";
+        }
+        i++;
+    }
+}
+
+void printOddMiddleRow(int num) {
+    int i = 0;
+    while (i < num) {
+        if (i % 2 == 0) {
+            std::cout << "1 ";
+        } else {
+            std::cout << "0 ";
+        }
+        i++;
+    }
+}
+
+void printMiddleRow(int num) {
+
+    if(isEven(num)) {
+        printEvenMiddleRow(num);
+    } else {
+        printOddMiddleRow(num);
+    }
+
+
+}
+void buildPattern(char str[]) {
 
     int num = charToNum(str);
 
-    int printSlots = 0; // variable for how many numbers can be printed in each side (left, right)
-
-    // outer loop for columns
+    int printSlots = 0; 
     for (int col = 0; col < num; col++) {
 
-        // if num is odd & col in first half, then increment blankSpace
-        if (num % 2 != 0 && col <= num / 2) {
+        if (isFirstHalf(num, col)) {
             printSlots++;
-        } else if (num % 2 == 0 && col < num / 2) { // if num is even
-            printSlots++;
-        } else { // if col is in second half, then decrement blankSpace
+
+        } else if (isEven(num) && col == num / 2) {
+
+        } else if (!isEven(num) && col == (num / 2)) {
+
+        } else { 
             printSlots--;
         }
+        if (!isNotMiddleRow(printSlots, num)) {
+
+            printMiddleRow(num);
+        }
         
-        // inner loop for rows
+        bool isOne = true;
+        bool isOneTwo = true;
+        
         for (int row = 0; row < num; row++) {
-
-
-            // if row is greater or lesser than printSlots, then just print blank 
+            
             if (row >= printSlots && row < num - printSlots) {
                 std::cout << "  ";
                 continue;
             }
 
-            // if row is in the left side
-            if (row <= num / 2) {
-                if (row % 2 == 0) {
-                    std::cout << "1 ";
-                } else {
-                    std::cout << "0 ";
-                }
-            } else { // if row is in the right side
-                if (col % 2 == 0 && row % 2 == 0) {
-                    std::cout << "1 ";
-                } else if (col % 2 == 0 && row % 2 != 0) {
-                    std::cout << "0 ";
-                } else if (row % 2 == 0) {
-                    std::cout << "0 ";
-                } else {
-                    std::cout << "1 ";
-                }
+            if (isNotMiddleRow(printSlots, num) && row < num / 2 && isOne) {
+                std::cout << "1 ";
+                isOne = !isOne;
+            } else if (isNotMiddleRow(printSlots, num) && row < num / 2) {
+                std::cout << "0 ";
+                isOne = !isOne;
+            } else if (isNotMiddleRow(printSlots, num) && isOneTwo) {
+                std::cout << "1 ";
+                isOneTwo = !isOneTwo;
+            } else if (isNotMiddleRow(printSlots, num)) {
+                std::cout << "0 ";
+                isOneTwo = !isOneTwo;
             }
         }
         std::cout << "\n";
