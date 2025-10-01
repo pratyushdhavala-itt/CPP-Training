e3f3ab4105f615b210dfe4de9ce56b9bf61e9277
Pratyush Dhavala <pratyush.v.d@intimetec.com>
2025-10-01 17:56:35 +0530

    Practice for 30-09-2025

--- practice/week8/30-09-2025/sets.cpp
+++ practice/week8/30-09-2025/sets.cpp
@@ -8,9 +8,12 @@ int main() {
     set<int> setOne;
 
     setOne = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+
+    set<int>::iterator it = setOne.begin();
+
+    setOne.erase(it);
+
+    cout << *it << endl;
     
-    for (auto i : setOne) {
-        cout << i << endl;
-    }
     return 0;
 }
--- practice/week8/30-09-2025/vector_algorithms.cpp
+++ practice/week8/30-09-2025/vector_algorithms.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void print(int x) {cout << x << endl;}
 int main() {
     
-    vector<int> vecOne{1, 2, 2, 3, 3, 3, 4, 6};
+    vector<int> vecOne{99, 56, 47, 32, 20, 11, 5, 0};
 
     // int x = 1;
     // for_each(vecOne.begin(), vecOne.end(), [x](int) {cout << x << endl;});
@@ -15,10 +15,36 @@ int main() {
 
     //auto x = count_if(vecOne.begin(), vecOne.end(), [](int x){return x % 2 == 0;});
 
-    auto it = find(vecOne.begin(), vecOne.end(), 6);
+    // auto it = find(vecOne.begin(), vecOne.end(), 7);
 
-    
+    // cout << it << endl;
+
+    // cout << *vecOne.end() << endl;
+
+    // cout << boolalpha << (it == vecOne.end()) << endl;
+
+    // cout << typeid(*it).name() << endl;
 
     // cout << x << endl;
+
+    // vector<int>::iterator it = find_if(vecOne.begin(), vecOne.end(), [](int x) {return x % 2 == 0;});
+
+    // cout << *it << endl;
+
+    // for_each(vecOne.begin(), vecOne.end(), [](int x){ cout << x << endl; });
+
+    // sort(vecOne.begin(), vecOne.end());
+
+    // for_each(vecOne.begin(), vecOne.end(), [](int x){ cout << x << endl; });
+
+
+    vector<pair<int, int>> vecTwo {{1, 2}, {3, 1}, {2, 2}, {2, 1}, {3, 0}};
+
+    // for_each(vecTwo.begin(), vecTwo.end(), [](pair<int, int>& p){ cout << p.first << " " << p.second << endl;});
+
+    sort(vecTwo.begin(), vecTwo.end(), [](pair<int, int>& p1, pair<int, int>& p2){ return p1.first < p2.first; });
+
+    for_each(vecTwo.begin(), vecTwo.end(), [](pair<int, int>& p){ cout << p.first << " " << p.second << endl;});
+
     return 0;
 }
